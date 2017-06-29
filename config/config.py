ac0d500f1ee68ba408bb8d6f78319a60560cface
Vladimir Burdukov <chippcheg@gmail.com>
2017-06-29 10:31:50 +0300

    updated config

--- config/config.py
+++ config/config.py
@@ -5,22 +5,18 @@ feeds = [
     ("Персонально ваш", "http://echo.msk.ru/programs/personalnovash/rss-audio.xml"),
     ("Ганапольское", "http://www.echo.msk.ru/programs/ganapolskoe_itogi/rss-audio.xml"),
     ("Кейс", "http://www.echo.msk.ru/programs/keys/rss-audio.xml"),
-    ("Альбац", "http://www.echo.msk.ru/contributors/7/rss-audio.xml"),
+    ("Альбац", "http://echo.msk.ru/programs/albac/rss-audio.xml"),
     ("Код доступа", "http://www.echo.msk.ru/programs/code/rss-audio.xml"),
-    ("Все так", "http://www.echo.msk.ru/programs/vsetak/rss-audio.xml"),
-    ("Все так +", "http://www.echo.msk.ru/programs/vsetakplus/rss-audio.xml"),
-    ("Вот так", "http://echo.msk.ru/programs/vottak/rss-audio.xml"),
-    ("Дилентанты", "http://echo.msk.ru/programs/Diletanti/rss-audio.xml"),
     ("Особое мнение", "http://echo.msk.ru/programs/personalno/rss-audio.xml"),
     ("2017", "http://www.echo.msk.ru/programs/year2017/rss-audio.xml"),
     ("Интервью", "http://www.echo.msk.ru/programs/beseda/rss-audio.xml"),
     ("48 минут", "http://www.echo.msk.ru/programs/48minut/rss-audio.xml"),
-    ("Разбор полета", "http://www.echo.msk.ru/programs/razbor_poleta/rss-audio.xml")
+    ("Невзоровские среды", "http://echo.msk.ru/programs/nevsredy/rss-audio.xml")
 ]
 
 settings = {
     "info": {
-        "title": u"Эхо Москвы",
+        "title": u"Эхо Москвы – Политика",
         "description": u"Правильный, комбинированный фид избранных передач (версия 2)",
         "link": "http://echo.msk.ru"
     },

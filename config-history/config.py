a8c1fc583343b11ceba36c04383db90c7733cca3
Vladimir Burdukov <chippcheg@gmail.com>
2017-07-18 12:14:53 +0300

    Updated configs

--- config-history/config.py
+++ config-history/config.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 
 feeds = [
-    ("Все так", "http://www.echo.msk.ru/programs/vsetak/rss-audio.xml"),
-    ("Все так +", "http://www.echo.msk.ru/programs/vsetakplus/rss-audio.xml"),
-    ("Вот так", "http://echo.msk.ru/programs/vottak/rss-audio.xml"),
-    ("Дилентанты", "http://echo.msk.ru/programs/Diletanti/rss-audio.xml"),
-    ("Разбор полета", "http://www.echo.msk.ru/programs/razbor_poleta/rss-audio.xml"),
-    ("48 минут", "http://www.echo.msk.ru/programs/48minut/rss-audio.xml"),
-    ("A-Team", "http://www.echo.msk.ru/programs/a_team/rss-audio.xml")
+    ("Все так",         "http://echo.msk.ru/programs/vsetak/rss-audio.xml"),
+    ("Все так +",       "http://echo.msk.ru/programs/vsetakplus/rss-audio.xml"),
+    ("Вот так",         "http://echo.msk.ru/programs/vottak/rss-audio.xml"),
+    ("Дилентанты",      "http://echo.msk.ru/programs/Diletanti/rss-audio.xml"),
+    ("Разбор полета",   "http://echo.msk.ru/programs/razbor_poleta/rss-audio.xml"),
+    ("48 минут",        "http://echo.msk.ru/programs/48minut/rss-audio.xml"),
+    ("A-Team",          "http://echo.msk.ru/programs/a_team/rss-audio.xml")
 ]
 
 settings = {
--- config-politics/config.py
+++ config-politics/config.py
@@ -1,15 +1,16 @@
 # coding=utf-8
 
 feeds = [
-    ("Персонально ваш", "http://echo.msk.ru/programs/personalnovash/rss-audio.xml"),
-    ("Ганапольское", "http://www.echo.msk.ru/programs/ganapolskoe_itogi/rss-audio.xml"),
-    ("Кейс", "http://www.echo.msk.ru/programs/keys/rss-audio.xml"),
-    ("Альбац", "http://echo.msk.ru/programs/albac/rss-audio.xml"),
-    ("Код доступа", "http://www.echo.msk.ru/programs/code/rss-audio.xml"),
-    ("Особое мнение", "http://echo.msk.ru/programs/personalno/rss-audio.xml"),
-    ("2017", "http://www.echo.msk.ru/programs/year2017/rss-audio.xml"),
-    ("Интервью", "http://www.echo.msk.ru/programs/beseda/rss-audio.xml"),
-    ("Невзоровские среды", "http://echo.msk.ru/programs/nevsredy/rss-audio.xml")
+    ("Персонально ваш",     "http://echo.msk.ru/programs/personalnovash/rss-audio.xml"),
+    ("Ганапольское",        "http://echo.msk.ru/programs/ganapolskoe_itogi/rss-audio.xml"),
+    ("Кейс",                "http://echo.msk.ru/programs/keys/rss-audio.xml"),
+    ("Альбац",              "http://echo.msk.ru/programs/albac/rss-audio.xml"),
+    ("Код доступа",         "http://echo.msk.ru/programs/code/rss-audio.xml"),
+    ("Особое мнение",       "http://echo.msk.ru/programs/personalno/rss-audio.xml"),
+    ("2017",                "http://echo.msk.ru/programs/year2017/rss-audio.xml"),
+    ("Интервью",            "http://echo.msk.ru/programs/beseda/rss-audio.xml"),
+    ("Невзоровские среды",  "http://echo.msk.ru/programs/nevsredy/rss-audio.xml"),
+    ("Без посредников",     "http://echo.msk.ru/programs/nomed/rss-audio.xml")
 ]
 
 settings = {
